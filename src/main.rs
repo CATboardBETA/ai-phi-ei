7dbf2846d64edfbada4fe935856e06bf034f6f67
CATboardBETA <catboardbeta@gmail.com>
2022-11-03 12:45:10 -0400

    temp
    Took 1 hour 21 minutes

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 //! [IPA](https://en.wikipedia.org/wiki/International_Phonetic_Alphabet) symbols.
 //!
 //! It also supports befunge-style arrows! Yay!
-
 pub mod cursor;
 
 use clap::{arg, value_parser, Arg, Command};
@@ -106,23 +105,54 @@ fn main() {
                         if x == "#" {
                             while let Some(x) = cursor.get_next() {
                                 if x == ";" {
+                                    cursor.move_forward();
                                     break;
                                 } else {
+                                    cursor.move_forward();
                                     stack.push(x.parse::<i32>().expect(&format!("Expected integer after #: {}, {}", cursor.x, cursor.y)));
                                 }
                             }
                         } else if x == "&" {
-                            cursor.move_
                             while let Some(x) = cursor.get_next() {
                                 if x == ";" {
+                                    cursor.move_forward();
                                     break;
                                 } else {
+                                    cursor.move_forward();
                                     input.push_str(&x);
                                 }
                             }
                         } else if x == ";" {
-                            panic!(&format!("Expected # or & after θ: {}, {}", cursor.x, cursor.y));
-                        } else if x == ""
+                            panic!("Expected # or & or command after θ: {}, {}", cursor.x, cursor.y);
+                        } else if x == "ð" {
+                            cursor.move_forward();
+                            // This would just pop from the stack and immediately push it back on,
+                            // so it's useless
+                        } else if x == "ð̠" {
+                            cursor.move_forward();
+                            let mut stack_push = vec![];
+                            while let x = stack.pop() {
+                                if x == None {
+                                    // We've reached the end of the stack
+                                    panic!("Stack underflow: {}, {}", cursor.x, cursor.y);
+                                }
+                                // It's safe to unwrap here because we just checked if it's None
+                                let x = x.unwrap();
+
+                                if x == 0 {
+                                    // End of the string, assuming it's actually a string we're
+                                    // reading, not just a bunch of random numbers
+                                    //
+                                    // We don't need to push the 0 back on the stack because it's
+                                    // the end of the string
+                                    break;
+                                } else {
+                                    stack_push.push(x);
+                                }
+                            }
+                            // We push only the first character of the string, because θ only
+                            // pushes one character/integer at a time
+                        }
                     }
                 }
                 _ => {
